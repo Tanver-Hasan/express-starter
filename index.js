35f3186e697158a2d8152b119d76b1c9aaafb876
Tanver-Hasan <38843176+Tanver-Hasan@users.noreply.github.com>
2025-11-18 10:41:48 +0000

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv');
+const port = process.env.PORT || 80;
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
